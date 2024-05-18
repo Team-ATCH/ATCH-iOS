e5edcd12948a1c14311f5e378bf08e429e924ff5
heejoo-sidewalk <heyjooo@sidewalkplay.com>
2024-05-18 13:44:51 +0900

    Initial Commit

---
+++ ATCH-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ATCH-iOS
+//
+//  Created by 변희주 on 5/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
