20a045649a0cb66ec972efa02fb95805e28434be
Heyjooo <heejoo_byun@naver.com>
2025-02-08 18:50:38 +0900

    Feat #36 - show toast message

--- ATCH-iOS/Extension/UIWindow+.swift
+++ ATCH-iOS/Extension/UIWindow+.swift
@@ -15,4 +15,38 @@ extension UIWindow {
             return UIApplication.shared.keyWindow
         }
     }
+    
+    func showToastMessage(message: String) {
+        DispatchQueue.main.async {
+            let toastLabel = UILabel()
+            toastLabel.numberOfLines = 0
+            toastLabel.font = .font(.body)
+            toastLabel.textColor = .atchWhite
+            toastLabel.text = message
+            
+            let backgroundView = UIView().then {
+                $0.backgroundColor = .atchBlack.withAlphaComponent(0.6)
+            }
+            
+            (self.rootViewController as! UINavigationController).topViewController?.view.addSubview(backgroundView)
+            backgroundView.addSubview(toastLabel)
+            backgroundView.snp.makeConstraints {
+                $0.top.equalTo(((self.rootViewController as! UINavigationController).topViewController?.view.safeAreaLayoutGuide)!).offset(12)
+                $0.centerX.equalToSuperview()
+            }
+            
+            toastLabel.snp.makeConstraints {
+                $0.edges.equalToSuperview().inset(15)
+            }
+            
+            backgroundView.layer.cornerRadius = 8
+            
+            UIView.animate(withDuration: 2) {
+                backgroundView.alpha = 0
+            } completion: { _ in
+                backgroundView.removeFromSuperview()
+            }
+        }
+    }
+    
 }
